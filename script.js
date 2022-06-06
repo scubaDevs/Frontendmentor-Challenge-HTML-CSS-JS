cf325dba26799cdb1f0e10a7578f23661f34ecbe
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-06-06 09:00:06 -0300

    new

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const arrow = document.getElementsByClassName('arrowdown');
- arrow.setAttribute()
+
+document.addEventListener("click", e=>{
+  const isDropdown = e.target.matches('[data-dropdown-active]');
+  if(!isDropdown && e.target.closest("[data-dropdown]") != null )return
+  let currentDropdown;
+  let allDropdowns = document.querySelectorAll("[data-dropdown-active]");
+  let element;
+  
+  if(isDropdown){
+    currentDropdown = e.target.dataset.dropdownActive;
+    element = document.querySelector(`.${currentDropdown}`);
+    element.classList.toggle('active');
+    
+  }
+  
+  allDropdowns.forEach(dropdown=>{
+    if(dropdown.dataset.dropdownActive == currentDropdown){
+      console.log(currentDropdown);
+      return
+    }else{
+      document.querySelector(`.${dropdown.dataset.dropdownActive}`).classList.remove('active')
+    }
+  
+  })
+    
+  });
